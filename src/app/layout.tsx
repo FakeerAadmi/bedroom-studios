8e84933313c439b334bd70ed0f974af767fe69ee
Shreyas <shreyaskulthe125@gmail.com>
2026-07-20 07:24:40 +0530

    chore: migration cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,7 @@ export const metadata: Metadata = {
     siteName: 'Bedroom Studios',
     locale: 'en_US',
     type: 'website',
+    images: [{ url: '/og-image.jpg', width: 1200, height: 630 }],
   },
   twitter: {
     card: 'summary_large_image',
